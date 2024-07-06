8695d38c1fc586cb46bae62edf1e338d081e1f52
Jan <frackowiak147@gmail.com>
2024-07-06 18:47:27 +0200

    18:47

--- AzureWebsite.Api/PeopleController.cs
+++ AzureWebsite.Api/PeopleController.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
 using AzureWebsite.Api.Models;
 
 namespace AzureWebsite.Api.Controllers
